5652ce0b77b77b35426583b0b880165170a2c3eb
thinhnguyenphanduc <37327862+thinhnguyenphanduc@users.noreply.github.com>
2018-03-16 14:48:57 +0700

    Update main.js

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -10,20 +10,22 @@ $(document).ready(function() {
     $.getJSON("assets/data/menu.json", function (menu) {     
         const docMenu = createDocMenu(menu);
         $("#doc-menu").html(docMenu);
+        
+        /*Click on navigation bar*/
+        $("a").on("click", function (e) {
+            alert("Click on navigation bar");
+            handleHTML(e, this);
+        });
+
+        /*Clicl on links in Div_Content*/ 
+        $("#div_content").on("click","a", function (e) {
+            alert("Click on navigation bar #div_content");
+            handleHTML(e, this);
+        });
     });
  
     // Get default content   
-     /*Click on navigation bar*/
-    $("a").on("click", function (e) {
-        alert("Click on navigation bar");
-        handleHTML(e, this);
-    });
-
-    /*Clicl on links in Div_Content*/ 
-    $("#div_content").on("click","a", function (e) {
-        alert("Click on navigation bar #div_content");
-        handleHTML(e, this);
-    });
+    
 });
 
 /*(function(){
