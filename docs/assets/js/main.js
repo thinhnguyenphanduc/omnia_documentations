bc97199c664f8b7fda2482d14e86cf04562f8fde
thinhnguyenphanduc <37327862+thinhnguyenphanduc@users.noreply.github.com>
2018-03-16 14:34:20 +0700

    Update main.js

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
 /*Click on navigation bar*/
 $("a").on("click",
     function (e) {
-        console.log("Click on navigation bar");
+        alert("Click on navigation bar");
         handleHTML(e, this);
     });
 
@@ -27,7 +27,7 @@ $("a").on("click",
 $("#div_content").on("click",
     "a",
     function (e) {
-        console.log("Click on navigation bar #div_content");
+        alert("Click on navigation bar #div_content");
         handleHTML(e, this);
     });
 
