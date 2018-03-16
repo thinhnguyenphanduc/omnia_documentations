16b98e22131029f836ea9aff550b110882845679
Thinh <ngphanducthinh@gmail.com>
2018-03-16 13:33:01 +0700

    ok

---
+++ docs/assets/js/custom.js
@@ -0,0 +1,72 @@
+﻿function createDocMenu(documents) {
+    var docMenuNode = "<li><input id='countries' placeholder='Search...' class='form-control'/></li>";
+    var isActive = true;
+    documents.forEach(function (doc) {
+        if (isActive) {
+            docMenuNode += `<li class='active'> <a href='${doc.href}' data-documentation='${doc.slug}'>${
+                doc.name}</a>`;
+            isActive = false;
+        } else {
+            docMenuNode += `<li> <a href='${doc.href}' data-documentation='${doc.slug}'>${doc.name}</a>`;
+        }
+
+        if (doc.subTitle.length > 0) {
+            docMenuNode += "<ul class='nav doc-sub-menu'>";
+            doc.subTitle.forEach(function (subDoc) {
+                docMenuNode += `<li> <a href='${subDoc.href}' data-documentation='${subDoc.slug}'>${subDoc.name
+                    }</a></li>`;
+            });
+            docMenuNode += "</ul>";
+        }
+        docMenuNode += "</li>";
+
+    });
+    return docMenuNode;
+};
+
+// Handle loading html to div tag
+function handleHTML(e, pointer) {
+    const targetHref = $(pointer).attr("href");
+    var href = $(location).attr('href');
+    // Clear id-based element movement
+    const endPos = href.indexOf("/#");
+    if (endPos > 0) {
+        href = href.slice(0, (endPos + 1));
+    }
+
+    console.log(`data: ${$(pointer).attr("data-documentation")}`);
+    console.log(`language: ${$('#language').val()}`);
+
+    // Load html to div tag
+    if (targetHref.indexOf("#") < 0 && targetHref.indexOf("http") != 0) // In case, there is # or external url
+    {
+        e.preventDefault();
+        console.log("preventDefault");
+        $('#div_content').load(href + $(pointer).attr("data-documentation") + targetHref,
+            function (response, status) {
+                console.log("loadDivContent");
+                // Change src of img tags
+                $('#div_content img').each(function () {
+                    var url = $(this).attr('src');
+                    // More than one ../
+                    while (url.indexOf("../") >= 0) {
+                        url = url.replace("../", "");
+                    }
+
+                    // Append complete url
+                    url = href +
+                        $(pointer).attr("data-documentation") +
+                        "/Docs/" +
+                        $('#language').val() +
+                        "/" +
+                        url;
+                    $(this).attr('src', url);
+                });
+                $('#div_content').innerHTML = response;
+
+                // Set data-documentation to every a tag
+                $('#div_content a').each(function () {
+                    $(this).attr('data-documentation', $(pointer).attr("data-documentation"));
+                });
+            });
+    }
--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,47 +1,31 @@
 $(document).ready(function() {
-	
-	/* ===== Affix Sidebar ===== */
-	/* Ref: http://getbootstrap.com/javascript/#affix-examples */
-
-    	
-	$('#doc-menu').affix({
-        offset: {
-            //top: ($('#header').outerHeight(true) + $('#doc-header').outerHeight(true)) + 45,
-           // bottom: ($('#footer').outerHeight(true) + $('#promo-block').outerHeight(true)) + 75
-        }
-    });
-    
-    /* Hack related to: https://github.com/twbs/bootstrap/issues/10236 */
     $(window).on('load resize', function() {
         $(window).trigger('scroll'); 
     });
 
     /* Activate scrollspy menu */
-    $('body').scrollspy({target: '#doc-nav', offset: 100});
-    
-    /* Smooth scrolling */
-	$('a.scrollto').on('click', function(e){
-        //store hash
-        var target = this.hash;    
-        e.preventDefault();
-		$('body').scrollTo(target, 800, {offset: 0, 'axis':'y'});
-		
-	});
-	
-    
-    /* ======= jQuery Responsive equal heights plugin ======= */
-    /* Ref: https://github.com/liabru/jquery-match-height */
-    
-     $('#cards-wrapper .item-inner').matchHeight();
-     $('#showcase .card').matchHeight();
-     
-    /* Bootstrap lightbox */
-    /* Ref: http://ashleydw.github.io/lightbox/ */
+    $('body').scrollspy({ target: '#doc-nav', offset: 100 });
 
-    $(document).delegate('*[data-toggle="lightbox"]', 'click', function(e) {
-        e.preventDefault();
-        $(this).ekkoLightbox();
-    });    
+    // Create navigation menu
+    $.getJSON("../data/menu.json", function (menu) {     
+        const docMenu = createDocMenu(menu);
+        $("#doc-menu").html(docMenu);
+    });
 
+    // Get default content
 
 });
+
+
+/*Click on navigation bar*/
+$("a").on("click",
+    function (e) {
+        handleHTML(e, this);
+    });
+
+/*Clicl on links in Div_Content*/
+$("#div_content").on("click",
+    "a",
+    function (e) {
+        handleHTML(e, this);
+    });
