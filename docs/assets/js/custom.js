cf94dc14566d0b081a84e471e3d1d53773108209
thinhnguyenphanduc <37327862+thinhnguyenphanduc@users.noreply.github.com>
2018-03-19 10:40:31 +0700

    Update custom.js

--- docs/assets/js/custom.js
+++ docs/assets/js/custom.js
@@ -62,6 +62,8 @@ function handleHTML(e, pointer) {
     const endPos = href.indexOf("/#");
     if (endPos > 0) {
         href = href.slice(0, (endPos + 1));
+        // Clean current url
+        window.location = href + "/#";
     }
     var data_documentation = $(pointer).attr("data-documentation");    
     // Load html to div tag
