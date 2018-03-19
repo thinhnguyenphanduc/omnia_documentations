5f411def77113a304c3c64315ed3a2ae588333f8
thinhnguyenphanduc <37327862+thinhnguyenphanduc@users.noreply.github.com>
2018-03-19 10:41:21 +0700

    Update custom.js

--- docs/assets/js/custom.js
+++ docs/assets/js/custom.js
@@ -1,7 +1,7 @@
  function createDocMenu(documents) { 
     var docMenuNode = "<li><input id='documentations' placeholder='Search...' class='form-control'/></li>";
     var isActive = true;
-    documents.forEach(function (doc) {
+    documents.forEach(function (doc) { 
         if (isActive) {
             docMenuNode += `<li class='active'> <a class='nav-level1' href='${doc.href}' data-documentation='${doc.slug}'>${
                 doc.name}</a>`;
